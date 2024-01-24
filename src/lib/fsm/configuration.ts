69e258dc6cd7dcaf9c9df380196378c4f30d1b14
Nick / Tempus <56578594+NickDue@users.noreply.github.com>
2024-01-24 12:15:50 +0100

    Added all 13 words, with their corresponding 33 sub-signs

--- src/lib/fsm/configuration.ts
+++ src/lib/fsm/configuration.ts
@@ -4,7 +4,16 @@ enum States {Start = 0,
     Peg3 = 1, Peg4 = 2,
     Kh3 = 3, Peg1 = 4,
     HKnuckle = 5, HKnuckle2 = 6, HKnuckle3 = 7,
-    Vomit1 = 8, Vomit2 = 9
+    Vomit1 = 8, Vomit2 = 9,
+    MediRightOk = 10, MediLeftKnuckle = 11,
+    Give1 = 12, Give2 = 13,
+    Kan1 = 14, Kan2 = 15,
+    Skal1 = 16, Skal2 = 17,
+    PegDu = 18,
+    Tage1 = 19, Tage2 = 20,
+    Apotek1 = 21, Apotek2 = 22, Apotek3 = 23,
+    Tak1 = 24, Tak2 = 25, Tak3 = 26,
+    Help1 = 27, Help2 = 28,
 }
 
 enum Events {
@@ -12,6 +21,15 @@ enum Events {
     StartHaft = 2, HaftEnd = 3,
     StartHKnuckle = 4, HKnuckleMiddle = 5, HKnuckleEnd = 6,
     StartVomit = 7, VomitEnd = 8,
+    StartMedi = 9, MediEnd = 10,
+    StartDu = 11, DuEnd = 12,
+    StartGive = 13, GiveEnd = 14,
+    StartKan = 15, KanEnd = 16,
+    StartSkal = 17, SkalEnd = 18,
+    StartTage = 19, TageEnd = 20,
+    StartApotek = 21, ApotekMiddle = 22, ApotekEnd = 23,
+    StartTak = 24, TakMiddle = 25, TakEnd = 26,
+    StartHelp = 27, HelpEnd = 28,
     restart = 99,
 }
 
@@ -29,7 +47,26 @@ const EventLibrary : EventLib = {
     "Left:StopL - Right:HKnuckle" : Events.HKnuckleEnd,
     " - Right:HFladHori" : Events.StartVomit,
     " - Right:HFladHori2" : Events.VomitEnd,
-
+    " - Right:MediRightOk" : Events.StartMedi,
+    "Left:MediLeftKnuckle - Right:MediRightOk" : Events.MediEnd,
+    "Left:PegDu - " : Events.StartDu,
+    "Left:PegDu - Right:StopR" : Events.DuEnd,
+    "Left:Give1Left - Right:Give1Right" : Events.StartGive,
+    "Left:Give2Left - Right:Give2Right" : Events.GiveEnd,
+    " - Right:Kan1" : Events.StartKan,
+    "Left:StopL - Right:Kan2" : Events.KanEnd,
+    " - Right:Skal1" : Events.StartSkal,
+    "Left:StopL - Right:Skal2" : Events.SkalEnd,
+    "Left:Tage1 - " : Events.StartTage,
+    "Left:Tage2 - Right:StopR" : Events.TageEnd,
+    " - Right:Apotek1" : Events.StartApotek,
+    " - Right:Apotek2" : Events.ApotekMiddle,
+    "Left:StopL - Right:Apotek3" : Events.ApotekEnd,
+    "Left:Tak1 - " : Events.StartTak,
+    "Left:Tak2 - " : Events.TakMiddle,
+    "Left:Tak3 - Right:StopR" : Events.TakEnd,
+    "Left:Help1Left - Right:Help1Right" : Events.StartHelp,
+    "Left:Help2Left - Right:Help2Right" : Events.HelpEnd,
 }
 
 export { States, Events, EventLibrary };
--- src/lib/fsm/fsm.ts
+++ src/lib/fsm/fsm.ts
@@ -15,6 +15,15 @@ export class FiniteStateMachine extends StateMachine<States, Events>  {
             t(States.Peg1,    Events.restart,       States.Start, () => console.log("Restarted")),
             t(States.HKnuckle3,    Events.restart,       States.Start, () => console.log("Restarted")),
             t(States.Vomit2,    Events.restart,       States.Start, () => console.log("Restarted")),
+            t(States.MediLeftKnuckle,    Events.restart,       States.Start, () => console.log("Restarted")),
+            t(States.PegDu,    Events.restart,       States.Start, () => console.log("Restarted")),
+            t(States.Give2,    Events.restart,       States.Start, () => console.log("Restarted")),
+            t(States.Kan2,    Events.restart,       States.Start, () => console.log("Restarted")),
+            t(States.Skal2,    Events.restart,       States.Start, () => console.log("Restarted")),
+            t(States.Tage2,    Events.restart,       States.Start, () => console.log("Restarted")),
+            t(States.Apotek3,    Events.restart,       States.Start, () => console.log("Restarted")),
+            t(States.Tak3,    Events.restart,       States.Start, () => console.log("Restarted")),
+            t(States.Help2,    Events.restart,       States.Start, () => console.log("Restarted")),
 
 
             t(States.Start,    Events.StartJeg,       States.Peg3, () => console.log("First sign of Jeg")),
@@ -29,6 +38,35 @@ export class FiniteStateMachine extends StateMachine<States, Events>  {
 
             t(States.Start,    Events.StartVomit,     States.Vomit1, () => console.log("First sign of Vomit")),
             t(States.Vomit1,    Events.VomitEnd,      States.Vomit2, async () => this.wordFound("Kaste op")),
+
+            t(States.Start,    Events.StartMedi,      States.MediRightOk, () => console.log("First sign of Medi")),
+            t(States.MediRightOk,    Events.MediEnd,      States.MediLeftKnuckle, async () => this.wordFound("Medicin")),
+
+            t(States.Start,    Events.StartDu,        States.PegDu, () => console.log("First sign of Du")),
+            t(States.PegDu,    Events.DuEnd,          States.Start, async () => this.wordFound("Du")),
+
+            t(States.Start,    Events.StartGive,      States.Give1, () => console.log("First sign of Give")),
+            t(States.Give1,    Events.GiveEnd,        States.Give2, async () => this.wordFound("Give")),
+
+            t(States.Start,    Events.StartKan,       States.Kan1, () => console.log("First sign of Kan")),
+            t(States.Kan1,     Events.KanEnd,         States.Kan2, async () => this.wordFound("Kan")),
+
+            t(States.Start,    Events.StartSkal,      States.Skal1, () => console.log("First sign of Skal")),
+            t(States.Skal1,    Events.SkalEnd,        States.Skal2, async () => this.wordFound("Skal")),
+
+            t(States.Start,    Events.StartTage,      States.Tage1, () => console.log("First sign of Tage")),
+            t(States.Tage1,    Events.TageEnd,        States.Tage2, async () => this.wordFound("Tage")),
+
+            t(States.Start,    Events.StartApotek,    States.Apotek1, () => console.log("First sign of Apotek")),
+            t(States.Apotek1,    Events.ApotekMiddle,    States.Apotek2, () => console.log("Second sign of Apotek")),
+            t(States.Apotek2,    Events.ApotekEnd,       States.Apotek3, async () => this.wordFound("Apotek")),
+
+            t(States.Start,    Events.StartTak,       States.Tak1, () => console.log("First sign of Tak")),
+            t(States.Tak1,    Events.TakMiddle,       States.Tak2, () => console.log("Second sign of Tak")),
+            t(States.Tak2,    Events.TakEnd,          States.Tak3, async () => this.wordFound("Tak")),
+
+            t(States.Start,    Events.StartHelp,      States.Help1, () => console.log("First sign of Help")),
+            t(States.Help1,    Events.HelpEnd,        States.Help2, async () => this.wordFound("Hjælp")),
 
         ]);
 
